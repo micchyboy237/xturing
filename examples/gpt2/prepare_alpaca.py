5889c67cda3b66715f4973673652bc0ce84e5d58
Jethro Estrada <jethroestrada@Jethros-MacBook-Air.local>
2023-05-23 09:27:00 +0800

    Updates on alpaca gpt4 tagalog dataset

--- examples/gpt2/prepare_alpaca.py
+++ examples/gpt2/prepare_alpaca.py
@@ -6,6 +6,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from llama.preparing_your_dataset import preprocess_alpaca_json_data
 
 file_name = "alpaca_gpt4_data"
-folder_path = "examples/llama/"
-dataset_path = os.path.join(os.path.dirname(__file__), "../llama/", file_name + ".json")
+folder_path = "../llama/"
+dataset_path = folder_path + file_name + ".json"
 preprocess_alpaca_json_data(dataset_path, folder_path + file_name)
--- examples/gpt2/prepare_alpaca_ph.py
+++ examples/gpt2/prepare_alpaca_ph.py
@@ -4,10 +4,8 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from llama.preparing_your_dataset import preprocess_alpaca_json_data
-from openai_dataset.generate import generate_from_seed
-from openai_dataset.generate_ph import convert_to_tagalog
 
 file_name = "alpaca_gpt4_data_ph"
-folder_path = "examples/llama/"
-dataset_path = os.path.join(os.path.dirname(__file__), "../llama/", file_name + ".json")
+folder_path = "../llama/"
+dataset_path = folder_path + file_name + ".json"
 preprocess_alpaca_json_data(dataset_path, folder_path + file_name)
